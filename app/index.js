0c56be0cb16813eef28ff134daf857d1053f2540
Efremov Alexey <lexich121@gmail.com>
2015-05-06 23:21:02 +0300

    Init

---
+++ app/index.js
@@ -0,0 +1,65 @@
+"use strict";
+var yeoman = require("yeoman-generator");
+var chalk = require("chalk");
+var yosay = require("yosay");
+
+module.exports = yeoman.generators.Base.extend({
+  initializing: function () {
+    this.pkg = require("../package.json");
+  },
+
+  prompting: function () {
+    var done = this.async();
+
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      "Welcome to the world-class " + chalk.red("Lxjs") + " generator!"
+    ));
+
+    var prompts = [{
+      type: "confirm",
+      name: "someOption",
+      message: "Would you like to enable this option?",
+      default: true
+    }];
+
+    this.prompt(prompts, function (props) {
+      this.props = props;
+      // To access props later use this.props.someOption;
+
+      done();
+    }.bind(this));
+  },
+
+  writing: {
+    app: function () {
+      this.fs.copy(
+        this.templatePath("_package.json"),
+        this.destinationPath("package.json")
+      );
+    },
+
+    projectfiles: function () {
+      this.fs.copy(
+        this.templatePath("editorconfig"),
+        this.destinationPath(".editorconfig")
+      );
+      this.fs.copy(
+        this.templatePath("eslintrc"),
+        this.destinationPath(".eslintrc")
+      );
+      this.fs.copy(
+        this.templatePath("jscsrc"),
+        this.destinationPath(".jscsrc")
+      );
+      this.fs.copy(
+        this.templatePath("index.js"),
+        this.destinationPath("index.js")
+      );
+    }
+  },
+
+  install: function () {
+    this.installDependencies();
+  }
+});
---
+++ test/test-app.js
@@ -0,0 +1,25 @@
+"use strict";
+/* global describe, before, it */
+var path = require("path");
+var assert = require("yeoman-generator").assert;
+var helpers = require("yeoman-generator").test;
+//var os = require("os");
+
+describe("lxjs:app", function () {
+  before(function (done) {
+    helpers.run(path.join(__dirname, "../app"))
+      .withOptions({ skipInstall: true })
+      .withPrompts({ someOption: true })
+      .on("end", done);
+  });
+
+  it("creates files", function () {
+    assert.file([
+      "package.json",
+      ".editorconfig",
+      ".eslintrc",
+      "index.js",
+      ".jscsrc"
+    ]);
+  });
+});
